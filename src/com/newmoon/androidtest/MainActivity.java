bd5e134b87a05d96588ec9725a5965da7b93f1b2
wexl <wexl@live.com>
2014-07-13 14:54:21 +0800

    初始化提交

---
+++ src/com/newmoon/androidtest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.newmoon.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	/** 申明视图控件 */
+	private Button btn1;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	/**
+	 * 查找控件
+	 */
+	private void findView() {
+		btn1 = (Button) findViewById(R.id.btn1);
+	}
+
+	/**
+	 * 添加事件监听
+	 */
+	private void addListener() {
+		btn1.setOnClickListener(clickListener);
+	}
+
+	/**
+	 * Click事件监听器
+	 */
+	OnClickListener clickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.btn1: {
+              
+			}
+			}
+
+		}
+	};
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
